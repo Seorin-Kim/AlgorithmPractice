b208fb107aa3389ea2b929f67449c779ccf7e809
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2025-02-18 16:23:40 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,11 +1,19 @@
-n = int(input())
-cnt = 1
-cnt_six = 6
-count = 1
+# 1: 1          (1)
+# 2: 2 - 7      (6 =6*1)
+# 3: 8 - 19     (12=6*2)
+# 4: 20 - 37    (18=6*3)
+# 5: 38 - 61    (24=6*4)
 
-while n > cnt:
-    count += 1
-    cnt += cnt_six
+# n = 1 + 6 * (0+1+2+3+4+...)
+
+n = int(input())
+sum = 1
+cnt_six = 6
+num_layers = 1
+
+while n > sum:
+    num_layers += 1
+    sum += cnt_six
     cnt_six += 6
 
-print(count)
+print(num_layers)
