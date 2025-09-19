d6295e13828db4cb1dcafd09d09cfce41a547e1d
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2025-09-19 19:39:18 +0900

    [level 2] Title: 소수 찾기, Time: 809.53 ms, Memory: 279 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    from itertools import permutations
+    
+    nums = set()
+    for i in range(len(numbers)):
+        nums |= set(map(int, map("".join, permutations(list(numbers), i + 1))))
+    nums -= set(range(0, 2))
+    
+    for i in range(2, int(max(nums) ** 0.5) + 1):
+        nums -= set(range(i * 2, max(nums) + 1, i))
+
+    return len(nums)
