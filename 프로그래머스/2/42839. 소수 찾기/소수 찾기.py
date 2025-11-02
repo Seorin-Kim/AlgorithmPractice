fc23de7cbd6516f4dd9e750d044018ea4ce0053e
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2025-11-02 13:22:45 +0900

    [level 2] Title: 소수 찾기, Time: 722.33 ms, Memory: 279 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -3,10 +3,10 @@ def solution(numbers):
     
     nums = set()
     for i in range(len(numbers)):
-        nums |= set(map(int, map("".join, permutations(list(numbers), i + 1))))
-    nums -= set(range(0, 2))
+        nums |= set(map(int, map("".join, permutations(list(numbers), i+1))))
+    nums -= set(range(2))
     
-    for i in range(2, int(max(nums) ** 0.5) + 1):
-        nums -= set(range(i * 2, max(nums) + 1, i))
-
+    for i in range(2, int(max(nums)**0.5)+1):
+        nums -= set(range(i*2, max(nums)+1, i))
+    
     return len(nums)
